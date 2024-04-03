6aeeb20a3ee0f287d68ad9021415eac6306d914a
Cornelius <72729830+Corneliusluis@users.noreply.github.com>
2024-04-03 23:01:28 +0700

    Time: 26 ms (26.36%), Space: 12.4 MB (86.74%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: List[str]
         """
         
-        # return ["Fizz" * (not i % 3) + "Buzz" * (not i % 5) or str(i) for i in range(1, n+1)]
+        return ["Fizz" * (not i % 3) + "Buzz" * (not i % 5) or str(i) for i in range(1, n+1)]
         
 #         result = []
 #         base3 = 3
@@ -27,18 +27,3 @@ class Solution(object):
 #             else:
 #                 result.append(str(i))
 #         return result
-
-        f, b, fb = 'Fizz', 'Buzz', 'FizzBuzz'
-        arr = [str(x + 1) for x in range(n)]
-        
-        for i in range(2, n, 3):
-            arr[i] = f
-        
-        for i in range(4, n, 5):
-            arr[i] = b
-        
-        for i in range(14, n, 15):
-            arr[i] = fb
-        
-        return arr
-        
