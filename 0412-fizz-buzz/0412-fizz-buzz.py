503269072e9c4a078f2e0c595dabf3f90f4f96ea
Cornelius <72729830+Corneliusluis@users.noreply.github.com>
2024-04-03 22:56:40 +0700

    Time: 21 ms (66.18%), Space: 12.7 MB (9.49%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -7,24 +7,38 @@ class Solution(object):
         
         # return ["Fizz" * (not i % 3) + "Buzz" * (not i % 5) or str(i) for i in range(1, n+1)]
         
-        result = []
-        base3 = 3
-        base5 = 5
-        base15 = 15
+#         result = []
+#         base3 = 3
+#         base5 = 5
+#         base15 = 15
         
-        for i in xrange(1, n+1):
-            if i == base15:
-                result.append('FizzBuzz')                
-                base3 = base15 + 3
-                base5 = base15 + 5
-                base15 += 15
-            elif i == base3:
-                result.append('Fizz')
-                base3 += 3
-            elif i == base5:
-                result.append("Buzz")
-                base5 += 5
-            else:
-                result.append(str(i))
-        return result
+#         for i in xrange(1, n+1):
+#             if i == base15:
+#                 result.append('FizzBuzz')                
+#                 base3 = base15 + 3
+#                 base5 = base15 + 5
+#                 base15 += 15
+#             elif i == base3:
+#                 result.append('Fizz')
+#                 base3 += 3
+#             elif i == base5:
+#                 result.append("Buzz")
+#                 base5 += 5
+#             else:
+#                 result.append(str(i))
+#         return result
+
+        f, b, fb = 'Fizz', 'Buzz', 'FizzBuzz'
+        arr = [str(x + 1) for x in range(n)]
+        
+        for i in range(2, n, 3):
+            arr[i] = f
+        
+        for i in range(4, n, 5):
+            arr[i] = b
+        
+        for i in range(14, n, 15):
+            arr[i] = fb
+        
+        return arr
         
