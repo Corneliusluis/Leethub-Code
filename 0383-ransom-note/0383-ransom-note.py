258af579226dcffe304564f665fe1c5fb274b44d
Cornelius <72729830+Corneliusluis@users.noreply.github.com>
2024-04-08 18:01:42 +0700

    Time: 17 ms (98.99%), Space: 11.9 MB (25.19%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -16,4 +16,18 @@ class Solution(object):
         # using Counter Library, but implement it in one liner
         # return not collections.Counter(ransomNote) - collections.Counter(magazine)
         
+        # using count method
         return all(ransomNote.count(i) <= magazine.count(i) for i in set(ransomNote))
+        
+        # [WIP] using my own counter implementation based on dictionary method
+#         def myCounter(word):
+#             counter = {}
+
+#             for letter in word:
+#                 counter[letter] = word.count(letter)
+
+#             return counter
+
+#         if not [myCounter(ransomNote)[x] <= myCounter(magazine)[x] for x in myCounter(ransomNote) if x in myCounter(magazine)]:
+#             return False
+#         return all([myCounter(ransomNote)[x] <= myCounter(magazine)[x] for x in myCounter(ransomNote) if x in myCounter(magazine)])
