70711ff29ae1975c8e8fc8610e04b3154990d52e
Cornelius <72729830+Corneliusluis@users.noreply.github.com>
2024-04-12 00:44:29 +0700

    Time: 41 ms (53.54%), Space: 11.7 MB (37.76%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,6 +4,27 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        # converted int to str method
+        # return str(x) == str(x)[::-1]
         
-        return str(x) == str(x)[::-1]
+        # reversing the number 
+        
+        # negative number is excluded
+        if x < 0:
+            return False
+        
+        # begin reversing
+        reversed_num = 0
+        temp = x
+        
+        while temp != 0:
+            digit = temp % 10
+            reversed_num = reversed_num * 10 + digit
+            temp //= 10
+            
+        return x == reversed_num
+        
+        
+        
+        
         
